d7c154bcc56b20cdb43d798420aa1ad9b56de2b6
balaji-matta18 <balajimatta.in@gmail.com>
2026-05-11 18:14:13 +0530

    cleaned practiced statements

--- Hello.cs
+++ Hello.cs
@@ -4,30 +4,12 @@ using System;
 namespace HelloWorld
 {
 
-    struct Balaji{
-
-       public int n;
-       public string s;
-      
-    }
   public class Program
   {
     public static void Main(string[] args)
     {
 
-     string input = Console.ReadLine();
-      System.Console.WriteLine("Hello World! " + input);LdapStyleUriParser
-
-
-    
-
-    Balaji b;
-    b.n= 4;
-
-    b.s="Gakajh";
-
-    Console.WriteLine(b.n);
-        Console.WriteLine(b.s);
+      System.Console.WriteLine("Hello World! ");
 
 
     }
