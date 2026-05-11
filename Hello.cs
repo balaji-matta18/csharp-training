bec2390399fce7902cc92d3745a9bf2b2ee63fac
balaji-matta18 <balajimatta.in@gmail.com>
2026-05-11 18:11:26 +0530

    renamed file

---
+++ Hello.cs
@@ -0,0 +1,35 @@
+using System;
+
+
+namespace HelloWorld
+{
+
+    struct Balaji{
+
+       public int n;
+       public string s;
+      
+    }
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+
+     string input = Console.ReadLine();
+      System.Console.WriteLine("Hello World! " + input);
+
+
+    
+
+    Balaji b;
+    b.n= 4;
+
+    b.s="Gakajh";
+
+    Console.WriteLine(b.n);
+        Console.WriteLine(b.s);
+
+
+    }
+  }
+}
